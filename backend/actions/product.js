8d2ba8b79429a10238efbbb1bcecb21d5bf04e98
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2020-04-27 16:41:47 -0700

    Backend: Added mongo model to Mongo index, Commented code for find product with query

--- backend/actions/product.js
+++ backend/actions/product.js
@@ -1,6 +1,17 @@
+const { Product } = require('../models/index');
+
 module.exports = {
 
     getProducts: (req, res) => {
+        console.log(req.query);
+
+        // Product.find(req.query)
+        //     .then(data => {
+        //         res.send({ products: data });
+        //     })
+        //     .catch(err => res.status(500).send({ error: err }));
+
+
         res.send({ res: 'Success' });
     }
 
