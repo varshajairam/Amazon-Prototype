ab0a337a78df5088e844a17bb2ba6e85b4e4d270
nicolashanout <nicolas.hanout@gmail.com>
2020-04-27 01:19:35 -0700

    set up mongodb connection

--- backend/models/mongo/index.js
+++ backend/models/mongo/index.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      poolSize: 500,
+      bufferMaxEntries: 0,
+    });
+    console.log(`mongoDB Connected`);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = { connectDB };
