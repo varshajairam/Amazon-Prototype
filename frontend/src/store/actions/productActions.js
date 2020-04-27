76d4a5e2bd3ef683f0860c1899a5d0b64ce73f65
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2020-04-27 16:45:53 -0700

    Frontend: Display products from redux store, set filters on filter click

--- frontend/src/store/actions/productActions.js
+++ frontend/src/store/actions/productActions.js
@@ -1,12 +1,25 @@
 import { get } from '../../helpers/communicationHelper';
 
-export const getProducts = () => {
+export const getProducts = (data) => {
     return (dispatch) => {
+        let query = "/product/getProducts?";
 
-        get("/product/getProducts")
-            .then(response => response.data)
+        for (let key in data) {
+            if (data[key] != "")
+                query += key + "=" + data[key] + "&";
+        }
+
+        console.log('query', query);
+
+        get(query)
             .then(data => {
                 console.log('data', data);
+
+                dispatch({
+                    type: "SET_PRODUCTS",
+                    payload: data
+                });
+
             }).catch(err => console.log("Some Error Occurred!"));
     }
 }
