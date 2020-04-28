8e7c6b9b22bb64a75f7ff759e879fd6fb0052b1e
nicolashanout <nicolas.hanout@gmail.com>
2020-04-27 19:22:23 -0700

    added category actions and product actions

---
+++ backend/actions/category.js
@@ -0,0 +1,17 @@
+const {Category} = require('../models/mongo')
+ 
+const addCategory =async (req, res) => {
+    console.log("in addCategory");
+    const newCategory = new Category({name: req.body.name});
+    const result = await newCategory.save();
+    res.send(result);
+};
+
+const getCategories =async (req, res) => {
+    res.send(await Category.find());
+};
+
+module.exports = {
+  addCategory,
+  getCategories,
+};
--- backend/actions/product.js
+++ backend/actions/product.js
@@ -1,9 +1,22 @@
 const { Product } = require('../models/index');
 
-const addProduct = (req, res) => {
-    console.log(req.files);
-    
+const addProduct = async (req, res) => {
+  const newProduct = new Product({...req.body});
+  const result = await newProduct.save();
+  res.send(result);
   res.send(`added productsuccessfully, this is what you sent me`);
+};
+
+const updateProduct = async (req, res) => {
+  const newProduct = new Product({...req.body});
+  const result = await newProduct.save();
+  res.send(result);
+  res.send(`added product successfully, this is what you sent me`);
+};
+
+const deleteProduct = async (req, res) => {
+  const result = Product.deleteOne({id: req.id});
+  res.send(result);
 };
 
 module.exports = {
