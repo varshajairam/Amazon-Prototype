df2d3f5eac3923077ecf78c018ff6d14c442499a
Ronak Shah <ronakshah2895@gmail.com>
2020-04-30 22:37:28 -0700

    Added login component

--- frontend/src/common/Login/Login.js
+++ frontend/src/common/Login/Login.js
@@ -3,7 +3,28 @@ import './Login.css';
 
 function Login() {
   return (
-    <h1>Login</h1>
+    <div className="LOGIN ui card">
+      <div className="content">
+        <div className="header">Login</div>
+      </div>
+      <div className="content">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="inputUsername">
+              Username
+              <input type="text" id="inputUsername" name="username" placeholder="Username" required />
+            </label>
+          </div>
+          <div className="field">
+            <label htmlFor="inputPass">
+              Password
+              <input type="password" id="inputPass" name="password" placeholder="Password" required />
+            </label>
+          </div>
+          <button type="submit" className="ui primary button">Login</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
