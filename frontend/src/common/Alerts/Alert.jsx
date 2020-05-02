4ae18db252ff495112faf181c7c81d8ce9e2b9de
nicolashanout <nicolas.hanout@gmail.com>
2020-05-01 20:37:13 -0700

    added alert component

---
+++ frontend/src/common/Alerts/Alert.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Alert = (props) => {
+  const { alerts } = props;
+  if (alerts !== null && alerts.length > 0) {
+    return alerts.map((alert) => {
+      return (
+        <div key={alert.id} class={`ui ${alert.alertType} message`}>
+          <div class="header">{alert.msg}</div>
+        </div>
+      );
+    });
+  } else {
+    return <Fragment />;
+  }
+};
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return { alerts: state.alert };
+};
+
+export default connect(mapStateToProps)(Alert);
