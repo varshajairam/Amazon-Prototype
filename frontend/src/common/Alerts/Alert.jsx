3a1064003682af21a09eeba59358987dbd262da0
nicolashanout <nicolas.hanout@gmail.com>
2020-05-02 11:35:31 -0700

    fixed alerts

--- frontend/src/common/Alerts/Alert.jsx
+++ frontend/src/common/Alerts/Alert.jsx
@@ -5,15 +5,27 @@ import { connect } from 'react-redux';
 const Alert = (props) => {
   const { alerts } = props;
   if (alerts !== null && alerts.length > 0) {
-    return alerts.map((alert) => {
-      return (
-        <div key={alert.id} class={`ui ${alert.alertType} message`}>
-          <div class="header">{alert.msg}</div>
-        </div>
-      );
-    });
+    return (
+      <div
+        className="ui container"
+        style={{ marginTop: '.2rem', marginBottom: '.2rem' }}
+      >
+        {alerts.map((alert) => {
+          return (
+            <div key={alert.id} class={`ui ${alert.alertType} message`}>
+              <div class="header">{alert.msg}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
   } else {
-    return <Fragment />;
+    return (
+      <div
+        className="ui container"
+        style={{ marginTop: '.2rem', marginBottom: '.2rem' }}
+      ></div>
+    );
   }
 };
 
