5aac3f3137522fd93d6aa8897fb38527d1ec79f8
nicolashanout <nicolas.hanout@gmail.com>
2020-05-04 19:28:08 -0700

    implemented updating and deleting products

--- frontend/src/common/AddProduct/Images.jsx
+++ frontend/src/common/AddProduct/Images.jsx
@@ -19,11 +19,31 @@ const Images = ({ state, back, next, updateState }) => {
 
   useEffect(() => {
     setPreviewImages({
-      file1: images.file1 ? URL.createObjectURL(images.file1) : undefined,
-      file2: images.file2 ? URL.createObjectURL(images.file2) : undefined,
-      file3: images.file3 ? URL.createObjectURL(images.file3) : undefined,
-      file4: images.file4 ? URL.createObjectURL(images.file4) : undefined,
-      file5: images.file5 ? URL.createObjectURL(images.file5) : undefined,
+      file1: images.file1
+        ? URL.createObjectURL(images.file1)
+        : state.images
+        ? state.images[0]
+        : undefined,
+      file2: images.file2
+        ? URL.createObjectURL(images.file2)
+        : state.images
+        ? state.images[1]
+        : undefined,
+      file3: images.file3
+        ? URL.createObjectURL(images.file3)
+        : state.images
+        ? state.images[2]
+        : undefined,
+      file4: images.file4
+        ? URL.createObjectURL(images.file4)
+        : state.images
+        ? state.images[3]
+        : undefined,
+      file5: images.file5
+        ? URL.createObjectURL(images.file5)
+        : state.images
+        ? state.images[4]
+        : undefined,
     });
   }, []);
 
@@ -50,7 +70,7 @@ const Images = ({ state, back, next, updateState }) => {
     return (
       <div className="card">
         <div className="image">
-          {images[fileName] ? (
+          {previewImages[fileName] ? (
             <img src={previewImages[fileName]} />
           ) : (
             <div className="ui placeholder">
@@ -64,48 +84,99 @@ const Images = ({ state, back, next, updateState }) => {
           )}
         </div>
         <div className="extra">
-          <input
-            className="ui feild"
-            type="file"
-            accept="image/*"
-            onChange={(e) => {
-              setImages({
-                ...images,
-                [fileName]:
-                  e.target.value && e.target.value.trim() !== ''
-                    ? e.target.files[0]
-                    : undefined,
-              });
-              setPreviewImages({
-                ...previewImages,
-                [fileName]:
-                  e.target.value && e.target.value.trim() !== ''
-                    ? URL.createObjectURL(e.target.files[0])
-                    : undefined,
-              });
-              updateState({
-                [fileName]:
-                  e.target.value && e.target.value.trim() !== ''
-                    ? e.target.files[0]
-                    : undefined,
-              });
-            }}
-          />
-          {images[fileName] ? (
+          {previewImages[fileName] ? null : (
+            <input
+              className="ui feild"
+              type="file"
+              accept="image/*"
+              onChange={(e) => {
+                setImages({
+                  ...images,
+                  [fileName]:
+                    e.target.value && e.target.value.trim() !== ''
+                      ? e.target.files[0]
+                      : undefined,
+                });
+                setPreviewImages({
+                  ...previewImages,
+                  [fileName]:
+                    e.target.value && e.target.value.trim() !== ''
+                      ? URL.createObjectURL(e.target.files[0])
+                      : undefined,
+                });
+                updateState({
+                  [fileName]:
+                    e.target.value && e.target.value.trim() !== ''
+                      ? e.target.files[0]
+                      : undefined,
+                });
+              }}
+            />
+          )}
+          {previewImages[fileName] ? (
             <button
               className="ui fluid button"
               style={{ marginTop: '.2rem' }}
               onClick={() => {
                 setImages({
-                  ...images, [fileName]: undefined,
-                });
-                setPreviewImages({
-                  ...previewImages, [fileName]: undefined,
-                });
-                
-                updateState({
+                  ...images,
                   [fileName]: undefined,
                 });
+                setPreviewImages({
+                  ...previewImages,
+                  [fileName]: undefined,
+                });
+                if (!images[fileName] && previewImages[fileName]) {
+                  switch (fileName) {
+                    case 'file1':
+                      updateState({
+                        images: [state.images.filter(
+                          (img, index) => index !== 0
+                        )],
+                      });
+                      break;
+
+                    case 'file2':
+                      updateState({
+                        images: state.images.filter(
+                          (img, index) => index !== 1
+                        ),
+                      });
+
+                      break;
+
+                    case 'file3':
+                      updateState({
+                        images: state.images.filter(
+                          (img, index) => index !== 2
+                        ),
+                      });
+
+                      break;
+
+                    case 'file4':
+                      updateState({
+                        images: state.images.filter(
+                          (img, index) => index !== 3
+                        ),
+                      });
+
+                      break;
+
+                    case 'file5':
+                      updateState({
+                        images: state.images.filter(
+                          (img, index) => index !== 4
+                        ),
+                      });
+
+                      break;
+                  }
+                } else {
+                  updateState({
+                    [fileName]: undefined,
+                  });
+                }
               }}
             >
               Remove Image
