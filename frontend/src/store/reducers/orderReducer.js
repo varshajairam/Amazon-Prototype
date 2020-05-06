40d5a70d9867d89557386da66460b620fced6774
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2020-05-05 20:41:19 -0700

    Backend / frontend: change property of return object to orders instead of products

--- frontend/src/store/reducers/orderReducer.js
+++ frontend/src/store/reducers/orderReducer.js
@@ -1,9 +1,17 @@
+import { GET_ORDERS_SUCCESS } from '../actions/types';
+
 const initialState = {
   orders: []
 };
 
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.payload.orders
+      };
+
     default:
       return {
         ...state,
