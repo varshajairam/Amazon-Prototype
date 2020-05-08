30a56817867e69fc5a31942327b298be3c829b98
nicolashanout <nicolas.hanout@gmail.com>
2020-05-07 18:07:17 -0700

    updated header

--- frontend/src/common/Header/Header.js
+++ frontend/src/common/Header/Header.js
@@ -10,17 +10,21 @@ function Header() {
   return (
     <div className="HEADER">
       {authReducerData.loggedIn && (
-        <div className="ui secondary menu">
+        <div className="ui secondary icon menu">
+        <div className="item"><i className="large yellow active amazon icon"></i></div>
           <NavLink exact className="item" activeClassName="active" to="/">Home</NavLink>
+          <NavLink exact className="item" activeClassName="active" to="/orderDetails">TEST</NavLink>
           <NavLink exact className="item" activeClassName="active" to="/productlist">Products</NavLink>
-          <NavLink exact className="item" activeClassName="active" to="/addProduct">Add Product</NavLink>
+          {authReducerData.user_type === 'Seller' && <NavLink exact className="item" activeClassName="active" to="/addProduct">Add Product</NavLink>}
           <NavLink exact className="item" activeClassName="active" to="/orders">Orders</NavLink>
           <Link onClick={() => dispatch(authActions.logout())} className="item right" to="/">Logout</Link>
         </div>
       )}
       {!authReducerData.loggedIn && (
-        <div className="ui secondary menu">
+        <div className="ui secondary icon menu">
+        <div className="item"><i className="large yellow active amazon icon"></i></div>
           <NavLink exact className="item" activeClassName="active" to="/">Home</NavLink>
+          <NavLink exact className="item" activeClassName="active" to="/orderDetails">TEST</NavLink>
           <NavLink exact className="item right" activeClassName="active" to="/login">Login</NavLink>
         </div>
       )}
